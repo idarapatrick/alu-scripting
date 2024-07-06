6159a75be6ed41cd9f698e1220cd5e530fbebf5a
idarapatrick <essienidara67@gmail.com>
2024-07-06 19:07:34 +0200

    modified

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,30 +1,17 @@
-# #!/usr/bin/python3
-# """Return number of subscribers for a given subreddit"""
-# import requests
-
-
-# def number_of_subscribers(subreddit):
-#     """Return the number of subscribers """
-#     url = "https://www.reddit.com/r/{}/about.json" \
-#         .format(subreddit)
-#     headers = {'User-Agent': 'My User Agent 1.0'}
-#     response = requests.get(url, headers=headers)
-#     if response.status_code == 200:
-#         return response.json().get('data') \
-#             .get('subscribers')
-#     return 0
+#!/usr/bin/python3
+"""Return the number of subscribers of a given subreddit"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given subreddit."""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'My User Agent 1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
 
-    # Check if the request was successful and not redirected
-    if response.status_code == 200:
-        data = response.json().get('data', {})
-        return data.get('subscribers', 0)
-    return 0
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
+
+    except Exception:
+        return 0
--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,41 +1,24 @@
 #!/usr/bin/python3
-# """Script that fetch 10 hot post for a given subreddit."""
-# import requests
-
-
-# def top_ten(subreddit):
-#     """Return number of subscribers if @subreddit is valid subreddit.
-#     if not return 0."""
-
-#     headers = {'User-Agent': 'MyAPI/0.0.1'}
-#     subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-#     response = requests.get(subreddit_url, headers=headers)
-
-#     if response.status_code == 200:
-#         json_data = response.json()
-#         for i in range(10):
-#             print(
-#                 json_data.get('data')
-#                 .get('children')[i]
-#                 .get('data')
-#                 .get('title')
-#             )
-#     else:
-#         print(None)
-
+"""Script that fetch 10 hot post for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts for a given subreddit."""
+    """Return number of subscribers if @subreddit is valid subreddit.
+    if not return 0."""
+
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = f"https://reddit.com/r/{subreddit}/hot.json"
-    response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
+    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
+    response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
         json_data = response.json()
-        posts = json_data.get('data', {}).get('children', [])
-        for i in range(min(10, len(posts))):
-            print(posts[i].get('data', {}).get('title', 'No title'))
+        for i in range(10):
+            print(
+                json_data.get('data')
+                .get('children')[i]
+                .get('data')
+                .get('title')
+            )
     else:
         print(None)
