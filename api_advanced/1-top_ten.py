e9b9430449b4fe8d34fbfc48dbc42aa0d462d005
idarapatrick <essienidara67@gmail.com>
2024-07-23 01:52:30 +0200

    PEP8

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-"""Script that fetches the first 10 hot posts for a given subreddit."""
+"""Prints the title of the first 10 hot posts listed for a given subreddit"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the first 10 hot posts listed for a given subreddit."""
-    headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = (
-        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    )
+    """Main function"""
+    URL = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
     try:
-        response = requests.get(subreddit_url, headers=headers)
-        if response.status_code == 200:
-            json_data = response.json()
-            posts = json_data.get('data', {}).get('children', [])
-            if not posts:
-                print(None)
-            for post in posts:
-                print(post.get('data', {}).get('title'))
-        else:
-            print(None)
-    except requests.RequestException:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        HOT_POSTS = RESPONSE.json().get("data").get("children")
+        [print(post.get('data').get('title')) for post in HOT_POSTS]
+    except Exception:
         print(None)
