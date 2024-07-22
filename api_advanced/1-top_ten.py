3edb414ddd366c9fa5c8b0db0e1d957c17891b75
idarapatrick <essienidara67@gmail.com>
2024-07-23 01:41:41 +0200

    redirects

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Script that fetch 10 hot post for a given subreddit."""
+"""Script that fetches the first 10 hot posts for a given subreddit."""
 import requests
-"""check"""
 
 
 def top_ten(subreddit):
-    """Return number of subscribers if @subreddit is valid subreddit.
-    if not return 0."""
-
+    """Print the titles of the first 10 hot posts listed for a given subreddit."""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    subreddit_url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
 
-    if response.status_code == 200:
-        json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
-    else:
-        print("OK")
+    try:
+        response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            json_data = response.json()
+            posts = json_data.get('data', {}).get('children', [])
+            for post in posts:
+                print(post.get('data', {}).get('title'))
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
