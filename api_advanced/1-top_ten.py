deddb246cc9fba722ac01c495b48b8f9e32cffac
idarapatrick <essienidara67@gmail.com>
2024-07-23 01:46:30 +0200

    200

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,13 +6,17 @@ import requests
 def top_ten(subreddit):
     """Print the titles of the first 10 hot posts listed for a given subreddit."""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    subreddit_url = (
+        "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    )
 
     try:
-        response = requests.get(subreddit_url, headers=headers, allow_redirects=False)
+        response = requests.get(subreddit_url, headers=headers)
         if response.status_code == 200:
             json_data = response.json()
             posts = json_data.get('data', {}).get('children', [])
+            if not posts:
+                print(None)
             for post in posts:
                 print(post.get('data', {}).get('title'))
         else:
